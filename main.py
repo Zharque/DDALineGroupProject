19e440d33d07b9d14f55795b65435d11a77bffa0
Mint Cola <152369729+Zharque@users.noreply.github.com>
2025-04-29 07:14:42 +0800

    comment group members

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+"""
+DDA Line Algorithm implementation demo
+
+Group Project for CS46, GRAPHICS AND VISUAL COMPUTING
+
+Group Members:
+Rynz Daval
+Mikhaina Tiu
+Danice Vernette Arroyo
+
+Professor:
+Ma'am Joy Dy
+
+TO RUN:
+(in cmd, project directory)
+.\.venv\Scripts\activate
+python main.py
+"""
+
 import sys
 import pygame
 
